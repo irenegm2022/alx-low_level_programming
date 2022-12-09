5aec85bdadeb82cae24aa3ac95cd86606bac73a0
irenegm2022 <mirenegathoni@gmail.com>
2022-12-09 13:11:18 -0800

    insomnia

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,17 @@
-#include <stdio.h>                                                         
-#include <stdlib.h>                                                        
-#include <time.h>                                                          
-                                                                           
+#include <stdio.h>       
+#include <stdlib.h>                  
+#include <time.h>
+
 /**                                                                        
  * main - Entry point                                                      
  *                                                                         
  * Return: Always 0                                                        
  */                                                                        
 int main(void)                                                             
-{                                                                          
-        int n;                                                             
-        int d;                                                             
-                                                                           
+{                                                                         
+	int n;                                                             
+	int d;                                         
+                                           
         srand(time(0));                                                    
         n = rand() - RAND_MAX / 2;                                         
         d = n % 10;                                                        
