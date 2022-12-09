825ffb6ddccbea8103b61596a14b658c95e6eea6
irenegm2022 <mirenegathoni@gmail.com>
2022-12-09 13:17:53 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>                                                                                                                                     
+                                                                                                                                                       
+/**                                                                                                                                                    
+ * main - prints alphabet in lower and uppercase                                                                                                       
+ *                                                                                                                                                     
+ * Return: Always 0                                                                                                                                    
+ */                                                                                                                                                    
+int main(void)                                                                                                                                         
+{                                                                                                                                                      
+        char lc;                                                                                                                                       
+                                                                                                                                                       
+        for (lc = 'a'; lc <= 'z'; lc++)                                                                                                                
+                putchar(lc);                                                                                                                           
+                                                                                                                                                       
+        for (lc = 'A'; lc <= 'Z'; lc++)                                                                                                                
+                putchar(lc);                                                                                                                           
+                                                                                                                                                       
+        putchar('\n');                                                                                                                                 
+                                                                                                                                                       
+        return (0);
