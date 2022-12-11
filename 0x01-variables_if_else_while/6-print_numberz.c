213e9799f43bc00c6eb39a59a5619e2c765bd958
irenegm2022 <mirenegathoni@gmail.com>
2022-12-11 08:41:19 -0800

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,23 @@
-#include <stdio.h>                                                                                                                                     
-                                                                                                                                                       
-/**                                                                                                                                                    
- * main - print all strings from 0 to 9                                                                                                                
- *                                                                                                                                                     
- * Return: Always 0                                                                                                                                    
- */                                                                                                                                                    
-int main(void)                                                                                                                                         
-{                                                                                                                                                      
-        int num;                                                                                                                                       
-                                                                                                                                                       
-        for (num = 0; num < 10; num++)                                                                                                                 
-                printf("%d", num);                                                                                                                     
-                                                                                                                                                       
-        printf("\n");                                                                                                                                  
-                                                                                                                                                       
-        return (0);
+#include <stdio.h>
+
+/**
+ * main - prints base ten digits without char
+ *
+ * main - prints all single digit numbers of base 10 starting from 0,
+ *followed by a new line, using putchar
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
