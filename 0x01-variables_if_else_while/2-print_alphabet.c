612c2c5349c3e174c9a070fdae10cde3980f6242
irenegm2022 <mirenegathoni@gmail.com>
2022-12-12 00:24:05 -0800

    Print 2 alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,17 @@
-#include <stdio.h>                                                                                                                                     
-                                                                                                                                                       
-/**                                                                                                                                                    
- * main - print alhabets in lowercase                                                                                                                  
- *                                                                                                                                                     
- * Return: Always 0                                                                                                                                    
- */                                                                                                                                                    
-int main(void)                                                                                                                                         
-{                                                                                                                                                      
-        char letter;                                                                                                                                   
-                                                                                                                                                       
-        for (letter = 'a'; letter <= 'z'; letter++)                                                                                                    
-        {                                                                                                                                              
-                putchar(letter);                                                                                                                       
-        }                                                                                                                                              
-        putchar('\n');                                                                                                                                                                                                                                                                                   
-        return (0);                                                                                                                                    
+#include <stdio.h>
+
+/**
+ * main - print alhabets in lowercase
+ *
+ * Return: Always 0
+ * */
+int main(void)
+{
+	char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+	{
+ putchar(letter);
+        }
+        putchar('\n');
+        return (0);
 }
