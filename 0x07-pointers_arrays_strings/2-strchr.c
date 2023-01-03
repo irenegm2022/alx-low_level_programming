f037bf1127cb90b24c2ba7be8d59a719d2939ae5
irenegm2022 <mirenegathoni@gmail.com>
2023-01-03 12:34:55 -0800

    locate string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,35 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * *_strspn - gets the length of a prefix substring
- * @s: string to evaluate
- * @accept: string containing the list of characters to match in s
+ * main - check the code
  *
- * Return: the number of bytes in the initial segment
- * of s which consist only of bytes from accept
+ * Return: Always 0.
  */
-unsigned int _strspn(char *s, char *accept)
+int main(void)
 {
-	int i, j, f, flag;
+	char *s = "hello";
+	char *f;
 
-	f = 0;
+	f = _strchr(s, 'l');
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (f != NULL)
 	{
-		flag = 0;
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				f++;
-				flag = 1;
-			}
-		}
-		if (flag == 0)
-		{
-			return (f);
-		}
+		printf("%s\n", f);
 	}
-
 	return (0);
 }
