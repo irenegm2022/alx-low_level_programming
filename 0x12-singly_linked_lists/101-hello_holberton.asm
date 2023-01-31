615b09bd746d552473ec62330f3f1addc9dbbd13
irenegm2022 <mirenegathoni@gmail.com>
2023-01-31 12:03:18 -0800

    holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,8 +1,16 @@
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
 main:
-          mov   edi, format
-          xor   eax, eax
-          call  printf
-          mov   eax, 0
-          ret
-format: db `Hello, Holberton\n`,0i
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
 
